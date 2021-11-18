f94852301b9a8dd475336b2d72b83bf89d3c98f7
jaisonrobson <xd_jrg@hotmail.com>
2021-11-18 06:53:05 -0300

    adicionado bibliotecas ao projeto & utilizado biblioteca styled-components em App.jsx

--- src/views/app/App.jsx
+++ src/views/app/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import styled from 'styled-components'
+
+import Logo from './Logo'
+import List from 'views/todo/list/List'
+import Sidebar from 'views/todo/sidebar/Sidebar'
+
+const StyledApp = styled.div`
+    width: 100vw;
+    height: 100vh;
+`
 
 const App = () => {
     return (
-        <div>
-            Todo List FourSales
-        </div>
+        <StyledApp>
+            <Logo />
+            <Sidebar />
+            <List />
+        </StyledApp>
     )
 }
 
---
+++ src/views/app/Logo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Row, Col } from 'reactstrap'
+
+const Logo = () => {
+    return (
+        <Container fluid>
+            <Row>
+                <Col>
+                    FourSales To Do List
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Logo
