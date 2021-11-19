90c5f24bff1a33e738449227c2ece1d79b6ca923
jaisonrobson <xd_jrg@hotmail.com>
2021-11-19 13:48:51 -0300

    corrigido exportacao de constantes de acoes do reducer

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,10 +1,10 @@
+import { todoadd } from './todo'
+
 export const ACTIONS = {
     TODO_ADD: 'todo-add',
     TODO_TOGGLE: 'todo-toggle',
     TODO_DELETE: 'todo-delete',
 }
-
-import { todoadd } from './todo'
 
 export {
     todoadd,
