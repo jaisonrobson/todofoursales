2e66c0d4874b06627abc65c6097cd4eaf33898bf
jaisonrobson <xd_jrg@hotmail.com>
2021-11-27 02:50:47 -0300

    ajustado action TODO_DELETE no reducer para nao mutar o array

--- src/store/reducers/todo.js
+++ src/store/reducers/todo.js
@@ -8,7 +8,7 @@ export function todosReducer(state, action) {
             return { ...state, todos: [...state.todos, action.payload.todo] }
 
         case ACTIONS.TODO_DELETE:
-            return { ...state, todos: _.remove(state.todos, todo => todo.id === action.payload.todo.id) }
+            return { ...state, todos: _.filter(state.todos, todo => todo.id !== action.payload.todo.id) }
 
         case ACTIONS.TODO_TOGGLE:
             return { ...state, todos: _.map(state.todos, todo => todo.id === action.payload.todo.id ? { ...todo, complete: !todo.complete } : todo) }
