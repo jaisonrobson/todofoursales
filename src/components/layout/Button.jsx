2ac4b23a4877bdf36f3200c6b8cba1ff8fe29d07
jaisonrobson <xd_jrg@hotmail.com>
2021-11-27 22:19:35 -0300

    alterado Button para receber estilizacao da biblioteca externa styled

--- src/components/layout/Button.jsx
+++ src/components/layout/Button.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import { Button as ReactstrapButton } from 'reactstrap'
 
-const Button = (props) => <ReactstrapButton {...props} />
+const StyledButton = styled(({ textDecoration, textColor, buttonColor, activeColor, hoverColor, ...props }) => <ReactstrapButton {...props} />)`
+    ${({ textDecoration }) => textDecoration ? `text-decoration: ${textDecoration};` : ''}
+    ${({ textColor }) => textColor ? `color: ${textColor};` : ''}
+    ${({ buttonColor }) => buttonColor ? `background-color: ${buttonColor};` : ''}
+    ${({ buttonColor }) => buttonColor ? `border-color: ${buttonColor};` : ''}
+    
+    :hover {
+        ${({ hoverColor }) => hoverColor ? `background-color: ${hoverColor};` : ''}
+        ${({ hoverColor }) => hoverColor ? `border-color: ${hoverColor};` : ''}
+    }
+
+    :active, :focus {
+        ${({ activeColor }) => activeColor ? `background-color: ${activeColor};` : ''}
+        ${({ activeColor }) => activeColor ? `border-color: ${activeColor};` : ''}
+    }
+`
+
+const Button = (props) => <StyledButton {...props} />
 
 export default Button
