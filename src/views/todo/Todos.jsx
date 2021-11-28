82167916ed3a5fae6e66edea58e0eaf4a70a9a57
jaisonrobson <xd_jrg@hotmail.com>
2021-11-28 19:31:43 -0300

    feito requisito para selecao de categoria no menu lateral

--- src/views/todo/Todos.jsx
+++ src/views/todo/Todos.jsx
@@ -1,17 +1,29 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 import Categories from 'views/todo/categories/Categories'
 import Activities from 'views/todo/activities/Activities'
+import { CATEGORIES } from 'util/constants'
 
 const Todos = () => {
+    // eslint-disable-next-line no-unused-vars
+    const [listedCategory, setListedCategory] = useState(CATEGORIES.WORK)
+
+    const updateCategory = (newCategory) => (
+        setListedCategory(
+            (prevState) => prevState !== newCategory
+                ? newCategory
+                : prevState
+        )
+    )
+
     return (
         <Fragment>
             <aside className="sidebar">
-                <Categories />
+                <Categories listedCategory={listedCategory} updateCategory={updateCategory} />
             </aside>
 
             <main className="content">
-                <Activities />
+                <Activities listedCategory={listedCategory} />
             </main>
         </Fragment>
     )
--- src/views/todo/categories/Categories.jsx
+++ src/views/todo/categories/Categories.jsx
@@ -5,29 +5,29 @@ import _ from 'lodash'
 import { CATEGORIES } from 'util/constants'
 import Button from 'components/layout/Button'
 
-const Categories = () => {
-    return (
-        <div id="categories">
-            {
-                _.map(
-                    _.toArray(CATEGORIES),
-                    (category) =>
-                        <Button
-                            block
-                            key={category}
-                            size="lg"
-                            textDecoration="none"
-                            textColor="black"
-                            buttonColor="transparent"
-                            hoverColor="#a3b9cf"
-                            activeColor="#778899"
-                        >
-                            {category}
-                        </Button>,
-                )
-            }
-        </div>
-    )
-}
+const Categories = ({ listedCategory, updateCategory }) => (
+    <div id="categories">
+        {
+            _.map(
+                _.toArray(CATEGORIES),
+                (category) =>
+                    <Button
+                        block
+                        key={category}
+                        size="lg"
+                        textDecoration="none"
+                        textColor="black"
+                        buttonColor="transparent"
+                        hoverColor="#a3b9cf"
+                        activeColor="#778899"
+                        onClick={() => updateCategory(category)}
+                        active={category === listedCategory}
+                    >
+                        {category}
+                    </Button>,
+            )
+        }
+    </div>
+)
 
 export default Categories
