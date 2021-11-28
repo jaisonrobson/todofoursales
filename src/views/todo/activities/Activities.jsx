bca0f12b4490a8baa274a210a9bb66c1068035b5
jaisonrobson <xd_jrg@hotmail.com>
2021-11-27 22:40:09 -0300

    feito esqueleto da estrutura inicial da listagem de afazeres em Activities.jsx

--- src/views/todo/activities/Activities.jsx
+++ src/views/todo/activities/Activities.jsx
@@ -1,13 +1,68 @@
 import './Activities.css'
 import React from 'react'
 
+import { Table } from 'reactstrap'
+
 import { withReducerContextConsumer } from 'contexts/withReducerContext'
 
 const Activities = ({ reducer }) => {
     console.log(`reducer`, reducer)
     return (
         <div id="activities">
-            Activities
+            <Table
+                hover
+                responsive
+                striped
+            >
+                <thead>
+                    <tr>
+                        <th>
+                            Nome
+                        </th>
+                        <th>
+                            Categoria
+                        </th>
+                        <th>
+                            Descricao
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            Sujeira na calha
+                        </td>
+                        <td>
+                            Pessoal
+                        </td>
+                        <td>
+                            Remover a sujeira da calha quando tiver tempo livre
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Sujeira na Piscina
+                        </td>
+                        <td>
+                            Pessoal
+                        </td>
+                        <td>
+                            Remover a sujeira da piscina antes da festa
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Requisito de Importacao
+                        </td>
+                        <td>
+                            Trabalho
+                        </td>
+                        <td>
+                            Averiguar melhor a necessidade da importacao de arquivos no sistema com o cliente
+                        </td>
+                    </tr>
+                </tbody>
+            </Table>
         </div>
     )
 }
