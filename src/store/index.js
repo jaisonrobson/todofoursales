15caeffb87a2a83413b62aa303f72005a1dce07b
jaisonrobson <xd_jrg@hotmail.com>
2021-11-28 18:50:28 -0300

    alimentado reducer com valores iniciais de afazeres

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,30 @@
+import { CATEGORIES } from 'util/constants'
 import { reducer } from './reducers'
 
 const initialState = {
-    todos: [],
+    todos: [
+        {
+            id: Date.now(),
+            name: 'Sujeira na calha',
+            category: CATEGORIES.PERSONAL,
+            description: 'Remover a sujeira da calha quando houver tempo livre',
+            completed: false,
+        },
+        {
+            id: Date.now() + 1,
+            name: 'Festa na piscina',
+            category: CATEGORIES.PERSONAL,
+            description: 'Higienizar a piscina antes da festa',
+            completed: false,
+        },
+        {
+            id: Date.now() + 2,
+            name: 'Requisito de importacao',
+            category: CATEGORIES.WORK,
+            description: 'Averiguar com o cliente a necessidade da importacao de arquivos no sistema',
+            completed: false,
+        },
+    ],
 }
 
 export {
