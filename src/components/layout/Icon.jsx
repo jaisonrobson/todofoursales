017c47ba15bc2bb531c75130357bf2f407c1f9af
jaisonrobson <xd_jrg@hotmail.com>
2021-11-28 04:30:21 -0300

    criado componente Icon para envolver biblioteca FontAwesome

---
+++ src/components/layout/Icon.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngry } from '@fortawesome/free-solid-svg-icons'
+
+const Icon = (props) => <FontAwesomeIcon icon={faAngry} {...props} />
+
+export default Icon
