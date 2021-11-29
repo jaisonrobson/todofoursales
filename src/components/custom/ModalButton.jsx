5a11ff670ff35e831b83e736821d1865de030d78
jaisonrobson <xd_jrg@hotmail.com>
2021-11-28 21:54:36 -0300

    criado componente ModalButton

---
+++ src/components/custom/ModalButton.jsx
@@ -0,0 +1,101 @@
+import React, { useContext } from 'react'
+import {
+    Modal as ReactstrapModal,
+    ModalHeader as ReactstrapModalHeader,
+    ModalBody as ReactstrapModalBody,
+    ModalFooter as ReactstrapModalFooter
+} from 'reactstrap'
+
+import { FlagContext, withFlagContext } from 'contexts/withFlagContext'
+import Button from 'components/layout/Button'
+
+const ModalFooter = ({
+    children,
+    showConfirmButton = true,
+    showCancelButton = true,
+    onConfirm = () => { },
+    ...rest
+}) => {
+    const { isOpen, setIsOpen } = useContext(FlagContext)
+    const toggleModal = () => setIsOpen(!isOpen)
+    const onConfirmInternal = () => {
+        const result = onConfirm()
+
+        if (result) toggleModal()
+
+        return result
+    }
+
+    return (
+        <ReactstrapModalFooter {...rest}>
+            {showConfirmButton ? (
+                <Button
+                    color="primary"
+                    onClick={onConfirmInternal}
+                >
+                    Salvar
+                </Button>
+            ) : null}
+
+            {children}
+
+            {showCancelButton ? (
+                <Button onClick={toggleModal}>
+                    Cancelar
+                </Button>
+            ) : null}
+        </ReactstrapModalFooter>
+    )
+}
+
+const ModalBody = ({ children, ...rest }) => (
+    <ReactstrapModalBody {...rest}>
+        {children}
+    </ReactstrapModalBody>
+)
+
+const ModalHeader = ({ children, ...rest }) => {
+    const { isOpen, setIsOpen } = useContext(FlagContext)
+    const toggleModal = () => setIsOpen(!isOpen)
+
+    return (
+        <ReactstrapModalHeader toggle={toggleModal} {...rest}>
+            {children}
+        </ReactstrapModalHeader>
+    )
+}
+
+const ModalButton = ({ children, buttonText, ...rest }) => {
+    const { isOpen, setIsOpen } = useContext(FlagContext)
+    const toggleModal = () => setIsOpen(!isOpen)
+
+    return (
+        <div>
+            <Button
+                onClick={toggleModal}
+                {...rest}
+            >
+                {buttonText}
+            </Button>
+
+            <ReactstrapModal
+                toggle={toggleModal}
+                isOpen={isOpen}
+            >
+                {children}
+            </ReactstrapModal>
+        </div>
+    )
+}
+
+ModalButton.Header = ModalHeader
+ModalButton.Body = ModalBody
+ModalButton.Footer = ModalFooter
+
+export default withFlagContext(
+    ModalButton,
+    {
+        flag: 'isOpen',
+        initialValue: false,
+    }
+)
